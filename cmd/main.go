6a0d16f6425b17ddccd35d8f51d013c6ba15e4de
Guilherme Felipe <guilherme.felipe@retta.com.br>
2024-08-14 09:32:11 -0300

    feat: adicionando suporte a env files e aplicando o Gracefully shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+
 	"github.com/GuiCezaF/todo-api/internal/handler/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
 	app := fiber.New()
 	routes.AddRoutes(app)
 
-	app.Listen(":3000")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		fmt.Println("Gracefully shutting down...")
+		app.Shutdown()
+	}()
+	if err := app.Listen(":3000"); err != nil {
+		panic(err)
+	}
 }
